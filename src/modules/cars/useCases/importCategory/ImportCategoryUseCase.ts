ed721b2ff31a7132965e2c9d7852920a5f29cb05
Gustavo Tempone de Oliveira Faria <gustavo.faria@efrete.com.br>
2022-05-25 14:36:30 -0300

    feat: csv imports

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,53 @@
+import fs from "fs";
+import { parse as csvParse } from "csv-parse";
+import { CategoriesRepository } from "../../repositories/Implementations/CategoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 class ImportCategoryUseCase {
-    execute(file: Express.Multer.File): void {
-        console.log(file);
+    constructor(private categoriesRepository: CategoriesRepository) {}
+
+    loadCateogries(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resole, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+            const parseFile = csvParse();
+
+            stream.pipe(parseFile);
+
+            parseFile
+                .on("data", async (line) => {
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description,
+                    });
+                })
+                .on("end", () => {
+                    resole(categories);
+                })
+                .on("erro", (err) => {
+                    reject(err);
+                });
+        });
+    }
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCateogries(file);
+
+        categories.map((category) => {
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if (!existCategory) {
+                this.categoriesRepository.create({
+                    name,
+                    description,
+                });
+            }
+        });
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { router } from "./routes";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 app.listen(3333, () => {
     console.log("Server started on port 3333");
